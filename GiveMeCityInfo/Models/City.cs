0a99ca12138efca8dee976aa127ac23c5d63afbb
BradleyCSO <Osborne.Bradley1998@gmail.com>
2022-12-23 11:43:19 +0000

    Add continent and country fields to City model

--- GiveMeCityInfo/Models/City.cs
+++ GiveMeCityInfo/Models/City.cs
@@ -5,5 +5,7 @@
         public int id { get; set; }
         public string? name { get; set; }
         public string? description { get; set; }
+        public string? continent { get; set; }
+        public string? country { get; set; }
     }
 }
