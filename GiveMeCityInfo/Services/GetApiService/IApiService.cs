2f2081daf15d2c34e0837f7d9633fc6f4fcf07b6
BradleyCSO <Osborne.Bradley1998@gmail.com>
2023-01-04 18:31:33 +0000

    Update the interface to be in line with ApiService implementation

--- GiveMeCityInfo/Services/GetApiService/IApiService.cs
+++ GiveMeCityInfo/Services/GetApiService/IApiService.cs
@@ -6,8 +6,10 @@ namespace GiveMeCityInfo.Services.GetApiService
     public interface IApiService
     {
         Task<List<City>> GetCities();
+        Task<PaginatedCities> GetFuzzyCities([FromQuery] string? searchQuery, string? pageNumber);
+        Task<List<Country>> GetCountries();
         Task<PaginatedCities> GetCitiesByCountry([FromQuery] string[]? countries, [FromQuery] string pageNumber);
         Task<City> GetCityById([FromQuery] string cityId);
-        Task<List<Country>> GetCountries();
+        Task<List<PointsOfInterest>> GetPointsOfInterestForCity([FromQuery] string cityId);
     }
 }
